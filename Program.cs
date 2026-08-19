5fb37fe3d5bfb77119b19c3c29dd943c1c8b1cb7
b3nj4dz <askdodh@gmail.com>
2026-08-19 17:57:54 -0400

    hola

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+Console.Write("Hola Mundo"); 
+Console.Write("Hola Mundo 2");
+// comentario
+/* bloque de comentario */
+Console.WriteLine();
+Console.WriteLine("Ingrese nombre: ");
+string? nombre=Console.ReadLine();
+Console.WriteLine("Su nombre es: "+nombre);
+int edad=21, peso=100;
+float estatura=1.80f;
+double promedio=7.0;
+bool estaVivo=true;
+char letra='1';
+Console.WriteLine("Ingrese edad: ");
+edad=int.Parse(Console.ReadLine()!);
+Console.WriteLine("El próximo año tendrá: "+(edad+1));
+int a=23;
+double b=a;
+double aa=2.3;
+int bb=(int)aa;
+string h=aa.ToString("f3"); //convertir en string
+Console.WriteLine("h="+h);
+bool estarVivo=int.TryParse("30",out int zz);
+Console.WriteLine(estarVivo);
+Console.WriteLine("zz="+(zz+1)); // +, -, /, *, %
+edad=edad+1;
+edad+=1;
+edad++;
